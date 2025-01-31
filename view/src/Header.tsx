53817138d2c979f94d00cea1eb21a16ecdb4acae
huszipe <petra.szilagyi@lego.com>
2025-01-31 13:29:17 +0100

    update: wrong 'POST' method, welcome component

--- view/src/Header.tsx
+++ view/src/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Header = () => {
   return (
     <div style={{ textAlign: "center", fontSize: "20px" }}>
-      Dear Codecooler! <br /> We have a challenge for you. Find the mistakes in
+      Dear Codecooler! <br /><br/> We have a challenge for you. Find the mistakes in
       the app and make it work!
     </div>
   );
--- view/src/Welcome.tsx
+++ view/src/Welcome.tsx
@@ -7,7 +7,7 @@ export const Welcome = () => {
 
   const fetchWelcome = async (): Promise<void> => {
     const response = await fetch("http://localhost:5000/", {
-      method: "GET",
+      method: "POST",
     });
 
     if (!response.ok) {
